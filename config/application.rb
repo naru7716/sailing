f7ed163113203e8aec21542799abf49cd627f66f
岡田成平 <7716.naruhei@gmail.com>
2020-12-17 09:24:46 +0900

    rspec導入

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Sailing
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    g.test_framework :rspec,
+      controller_specs: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
