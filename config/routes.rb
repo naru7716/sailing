894347b55af05671e9739887036b110c17b6bdb5
岡田成平 <7716.naruhei@gmail.com>
2020-12-22 20:54:04 +0900

    ログインページsessionコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :signup,       to: 'users#new'
+  get :login,        to: 'sessoin#new'
+  get :login,        to: 'session#create'
+  get :login,        to: 'sessoin#destroy'
   resources :users
 end
