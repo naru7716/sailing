800fdc7948f2ec66c1bfb58a3170f8f364e1a600
岡田成平 <7716.naruhei@gmail.com>
2020-12-24 19:24:28 +0900

    application_controllerにsessionコントローラー追記

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:enmail].downcase)
-    if user && user.authenticate(params[:sessoin][password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
