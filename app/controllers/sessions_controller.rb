cca898a318105bb1fc213e6b066f0457a4d69bcd
岡田成平 <7716.naruhei@gmail.com>
2020-12-24 18:14:06 +0900

    session_controller.rb追記

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:enmail].downcase)
+    if user && user.authenticate(params[:sessoin][password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'メールアドレスとパスワードの組み合わせが誤っています'
+      render 'new'
+    end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
