2e7722d82d847be8f53426cc92caf6708e86e27e
岡田成平 <7716.naruhei@gmail.com>
2021-01-02 16:55:51 +0900

    適切なユーザーにアクションを行わせるよう実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -43,5 +46,13 @@ class UsersController < ApplicationController
 
     def user_params_update
       params.require(:user).permit(:name, :email, :introduction, :sex)
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      if !current_user?(@user)
+        flash[:danger] = "このページへはアクセスできません"
+        redirect_to(root_url)
+      end
     end
 end
