fb8ae767097c41e042a182f971347ce30750c0a1
岡田成平 <7716.naruhei@gmail.com>
2021-01-05 13:45:51 +0900

    ユーザー削除機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    flash[:success] = "アカウントを削除しました"
+    redirect_to root_url
+  end
+
   private
 
     def user_params
