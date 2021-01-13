7a4d45d56477d82d1476a196c63441f8c35c5849
岡田成平 <7716.naruhei@gmail.com>
2021-01-14 01:18:42 +0900

    プロフィールページで投稿を表示するテスト追加

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -15,6 +15,22 @@ RSpec.describe "StaticPages", type: :system do
         expect(page).to have_title full_title
       end
     end
+
+    context "投稿フィード", js: true do
+      let!(:user) { create(:user) }
+      let!(:micropost) { create(:micropost, user: user) }
+
+      it "投稿のぺージネーションが表示されること" do
+        login_for_system(user)
+        create_list(:micropost, 6, user: user)
+        visit root_path
+        expect(page).to have_content "みんなの投稿 (#{user.microposts.count})"
+        expect(page).to have_css "div.pagination"
+        Micropost.take(5).each do |d|
+          expect(page).to have_link d.name
+        end
+      end
+    end
   end
 
   describe "ヘルプページ" do
