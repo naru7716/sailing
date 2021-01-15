ca34042128882a46b2e3ab8f2b542371c6fca8bc
岡田成平 <7716.naruhei@gmail.com>
2021-01-15 20:11:05 +0900

    投稿詳細ページレイアウト

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -11,7 +11,7 @@ class Micropost < ApplicationRecord
             :numericality => {
               :only_interger => true,
               :greater_than_or_equal_to => 1,
-              :less_than_or_equal_to => 5
+              :less_than_or_equal_to => 10
             },
             allow_nil: true
 end
