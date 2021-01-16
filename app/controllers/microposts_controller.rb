075621dfe28b57413b4b79b99fd491f18faf86e1
岡田成平 <7716.naruhei@gmail.com>
2021-01-16 16:52:16 +0900

    本人のみ投稿を編集できるように実装

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @micropost = Micropost.find(params[:id])
@@ -37,5 +38,10 @@ class MicropostsController < ApplicationController
 
     def micropost_params
       params.require(:micropost).permit(:name, :discription, :time, :team, :maintenance, :wind)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
