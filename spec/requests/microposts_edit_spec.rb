fd5d0f1fc69753d948e61c8ef7bc9bfef5cedc42
岡田成平 <7716.naruhei@gmail.com>
2021-01-16 16:55:39 +0900

    本人のみが料理の更新できるようにテスト

--- spec/requests/microposts_edit_spec.rb
+++ spec/requests/microposts_edit_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe "投稿編集", type: :request do
   let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
   let(:micropost) { create(:micropost, user: user) }
 
   context "認可されたユーザーの場合" do
@@ -34,6 +35,23 @@ RSpec.describe "投稿編集", type: :request do
                                                       time: 5} }
       expect(response).to have_http_status "302"
       expect(response).to redirect_to login_path
+    end
+  end
+
+  context "別アカウントのユーザーの場合" do
+    it "ホーム画面にリダイレクトすること" do
+      login_for_request(other_user)
+      get edit_micropost_path(micropost)
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to root_path
+      patch micropost_path(micropost), params: { micropost: { name: "大会１ヶ月前",
+                                                      description: "レース艇のセッティングの見直しを海上で行った。",
+                                                      team: "あああ大学",
+                                                      wind: "南南東",
+                                                      maintenance: "グースネック交換",
+                                                      time: 5} }
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to root_path
     end
   end
 end
