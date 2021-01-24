6caaa0e5da86dc970f15ba934210ce89118a116f
岡田成平 <7716.naruhei@gmail.com>
2021-01-24 14:55:31 +0900

    セッティングモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_124233) do
+ActiveRecord::Schema.define(version: 2021_01_23_184013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,15 @@ ActiveRecord::Schema.define(version: 2021_01_21_124233) do
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
+  create_table "settings", force: :cascade do |t|
+    t.string "name"
+    t.integer "micropost_id"
+    t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["micropost_id"], name: "index_settings_on_micropost_id"
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/factories/settings.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :setting do
+    name "MyString"
+    micropost_id 1
+    value "MyString"
+  end
+end
